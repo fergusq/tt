320ad2edecb79b7679d25ad47b4996248a92a97f
fergusq <fergusq@kaivos.org>
2013-03-15 14:23:46 +0200

    Added copyright notices

--- TextTools/src/org/kaivos/tt/parser/TextToolsParser.java
+++ TextTools/src/org/kaivos/tt/parser/TextToolsParser.java
@@ -1,3 +1,21 @@
+/*
+ * Text Tools Text generator
+ * Copyright (C) 2013 IH (fergusq)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.kaivos.tt.parser;
 
 import java.io.BufferedReader;
@@ -35,7 +53,7 @@ public class TextToolsParser {
 					case "-v":
 					case "--version":
 						System.out.println(
-								"TextTools (v0.1) - (c) 2013 IH (fergusq) - All rights reserved\n"
+								"TextTools (v0.1) - Copyright (C) 2013 IH (fergusq)"
 						);
 					case "--version-str":
 						System.out.println(
